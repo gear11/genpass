1fa8ca1e1c13591f03d5b2df479cfe048dcc90a9
Andy Jenkins <andy@gear11.com>
2022-08-18 15:27:28 -0400

    Add digit and special char behavior

--- genpass.py
+++ genpass.py
@@ -11,28 +11,55 @@ import hashlib
 import base64
 
 
+SPECIAL = r'@#*(){}+=/?~;,.-_'
+DIGITS = r'0123456789'
+
+
 def genpass(passphrase, domain, length=12, require_special=False, require_number=False):
-    h = hashlib.new('sha256')
-    h.update(f'{passphrase} {domain}\n'.encode())
-    b = h.digest()
-    password = base64.b64encode(b).decode()
+    h = hash(f'{passphrase} {domain}\n')
+    password = base64.b64encode(h).decode()
     if length and len(password) > length:
         password = password[0:length]
-    if require_special:
-        password = ensure_special(password)
-    if require_number:
-        password = ensure_number(password)
+    # Loop because if length is to be maintained, then overwriting can clobber a required character
+    while (require_special and not has_special(password)) or (require_number and not has_digit(password)):
+        print(f'Password is now {password}')
+        if require_special and not has_special(password):
+            password = ensure_special(password)
+        if require_number and not has_digit(password):
+            password = ensure_digit(password)
     return password
 
 
-def ensure_special(s):
+def hash(s):
+    h = hashlib.new('sha256')
+    h.update(s.encode())
+    return h.digest()
+
+
+def has_digit(password):
+    return any(elem in password for elem in DIGITS)
+
+
+def has_special(password):
+    return any(elem in password for elem in SPECIAL)
+
+
+def ensure_special(password):
     """Deterministically replace a character in S with a special character"""
-    return s
+    print("Ensure special")
+    prand = int.from_bytes(hash(f'salt for special {password}'), 'big')  # pseudo-random based on hash of password
+    chars = list(password)
+    chars[prand % len(chars)] = SPECIAL[prand % len(SPECIAL)]
+    return ''.join(chars)
 
 
-def ensure_number(s):
-    """Deterministically replace a character in S with a number"""
-    return s
+def ensure_digit(password):
+    """Deterministically replace a character in S with a digit"""
+    print("Ensure digit")
+    prand = int.from_bytes(hash(f'salt for digit {password}'), 'big')  # pseudo-random based on hash of password
+    chars = list(password)
+    chars[prand % len(chars)] = DIGITS[prand % len(DIGITS)]
+    return ''.join(chars)
 
 
 if __name__ == "__main__":
